9d805fd4529f6e8dd57e844a24489307d344fc13
Boaz Blake <boazblake@gmail.com>
2024-11-11 05:59:30 -0600

    using share to save on mobile

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "9b8b56950f3d9675b1bb0f6b5d263004"
   }, {
     "url": "index.html",
-    "revision": "0.7vh4gsqo4c8"
+    "revision": "0.jbtu4g7rqj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
