969a1d29940b6846dbedb4461a4297668c820775
Boaz Blake <boazblake@Boazs-MacBook-Air.local>
2025-06-19 13:52:52 -0500

    maybe nto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,14 +3,14 @@ import { CapacitorConfig } from "@capacitor/cli";
 const isDev = process.env.NODE_ENV === "development";
 
 const config: CapacitorConfig = {
-  appId: "io.ionic.liftmate",
+  appId: "io.boazblake.liftmate",
   appName: "Lift Mate",
   webDir: "docs", // Ensure this matches the directory where your built web assets are stored
   server: isDev
     ? {
-      url: "http://localhost:8101", // Only use this during development
-      cleartext: true, // Allow HTTP in development mode
-    }
+        url: "http://localhost:8101", // Only use this during development
+        cleartext: true, // Allow HTTP in development mode
+      }
     : {}, // Remove server URL in production
   ios: {
     webContentsDebuggingEnabled: true, // Enable web debugging for iOS
